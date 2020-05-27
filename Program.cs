e3505dd490284072eb33fab39b2bd7388cf7eea1
Rodrigo Martins <rodmajc@gmail.com>
2020-05-27 09:25:30 -0300

    Acrescentada a frase Tenha um bom dia

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ namespace projeto_teste {
 	class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Olá mundo!");
-			Console.WriteLine("Bom dia");
 			Console.WriteLine("Como você está?");
+			Console.WriteLine("Tenha um bom dia");
 			Console.ReadLine();
 		}
 	}
